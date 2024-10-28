4a01c9d334a2522fcf55c555de2a2fc64670f93a
tetsugo-mac <tetsugou2002@gmail.com>
2024-10-29 03:00:13 +0900

    .gitignore変更

--- convert_to_latex.py
+++ convert_to_latex.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def csv_to_latex(filepath:str) -> str:
-    df = pd.read_csv(filepath)
+    df  = pd.read_csv(filepath)
     df = df.dropna(how='all')
     df = df.dropna(axis=1, how='all')  
     latex_code = df.to_latex(index=False, escape=False, column_format= 'c' * len(df.columns))
