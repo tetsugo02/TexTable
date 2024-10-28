8dc3ca79ae073d0788ea10e666e12a14ce5f6a19
tetsugo-mac <tetsugou2002@gmail.com>
2024-10-29 01:29:29 +0900

    excelを変換する関数削除

--- convert_to_latex.py
+++ convert_to_latex.py
@@ -1,12 +1,5 @@
 import pandas as pd
 
-def excel_to_latex(filepath:str) -> str:
-    df = pd.read_excel(filepath)
-    df = df.dropna(how='all') #NaNになっている行を削除
-    df = df.dropna(axis=1, how='all')  
-    latex_code = df.to_latex(index=False, escape=False, column_format= 'c' * len(df.columns))
-    return latex_code
-
 def csv_to_latex(filepath:str) -> str:
     df = pd.read_csv(filepath)
     df = df.dropna(how='all')
