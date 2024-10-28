e9c83103efb108d8739834d42531916af4306bed
tetsugo-mac <tetsugou2002@gmail.com>
2024-10-29 00:45:34 +0900

    Nullのセルを削除

--- convert_to_latex.py
+++ convert_to_latex.py
@@ -2,10 +2,14 @@ import pandas as pd
 
 def excel_to_latex(filepath:str) -> str:
     df = pd.read_excel(filepath, engine='openpyxl')
+    df = df.dropna(how='all')  # Drop rows where all elements are NaN
+    df = df.dropna(axis=1, how='all')  # Drop columns where all elements are NaN
     latex_code = df.to_latex(index=False, escape=False)
     return latex_code
 
 def csv_to_latex(filepath:str) -> str:
     df = pd.read_csv(filepath)
+    df = df.dropna(how='all')  # Drop rows where all elements are NaN
+    df = df.dropna(axis=1, how='all')  # Drop columns where all elements are NaN
     latex_code = df.to_latex(index=False, escape=False)
     return latex_code
