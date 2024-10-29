59302a4b3b0a55a3c64ad70db66ea6c9bf343331
tetsugo-mac <tetsugou2002@gmail.com>
2024-10-29 12:46:05 +0900

    excel_readerのの修正

--- convert_to_latex.py
+++ convert_to_latex.py
@@ -1,17 +1,21 @@
-import pandas as pd
+from pandas import read_csv
 
-def csv_to_latex(filepath:str) -> str:
-    df  = pd.read_csv(filepath)
-    df = df.dropna(how='all')
-    df = df.dropna(axis=1, how='all')  
-    latex_code = df.to_latex(index=False, escape=False, column_format= 'c' * len(df.columns))
+
+def csv_to_latex(filepath: str) -> str:
+    df = read_csv(filepath)
+    df = df.dropna(how="all")
+    df = df.dropna(axis=1, how="all")
+    latex_code = df.to_latex(
+        index=False, escape=False, column_format="c" * len(df.columns)
+    )
     return add_header_footer(latex_code)
 
-def add_header_footer(latex_code:str) -> str:
-        begin:int = latex_code.find("\\begin{tabular}")
-        end:int = latex_code.rfind("\\end{tabular}") + len("\\end{tabular}")
-        body_code:str = latex_code[begin:end]
-        entire_talbe_code:str = (
+
+def add_header_footer(latex_code: str) -> str:
+    begin: int = latex_code.find("\\begin{tabular}")
+    end: int = latex_code.rfind("\\end{tabular}") + len("\\end{tabular}")
+    body_code: str = latex_code[begin:end]
+    entire_talbe_code: str = (
         "\\begin{table}[H]\n"
         "\\centering\n"
         "\\caption{}\n"
@@ -19,5 +23,4 @@ def add_header_footer(latex_code:str) -> str:
         f"{body_code}\n"
         "\\end{table}"
     )
-        return entire_talbe_code
-        
+    return entire_talbe_code
