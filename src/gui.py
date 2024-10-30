618acb3de4fe386575fac1584396229d9affd8f0
wsl_tetsugo <tetsugou2002@gmail.com>
2024-10-30 23:29:50 +0900

    ファイル分割し、可読性を上げ

--- src/gui.py
+++ src/gui.py
@@ -1,8 +1,7 @@
 import tkinter as tk
 from tkinter import filedialog, scrolledtext, ttk
 from TexTableConverter import TexTableConverter
-import subprocess
-import platform
+from copy_to_clipboard import copy_to_clipboard
 
 convert = TexTableConverter()
 
@@ -118,20 +117,6 @@ def update_output_and_copy(latex_code, message):
     copy_to_clipboard(latex_code)
     show_status_message(message)
 
-def copy_to_clipboard(text):
-    """クリップボードにコピー"""
-    try:
-        if platform.system() == "Darwin":
-            from AppKit import NSPasteboard, NSStringPboardType
-            pb = NSPasteboard.generalPasteboard()
-            pb.clearContents()
-            pb.setString_forType_(text, NSStringPboardType)
-        elif platform.system() == "Linux":
-            subprocess.run(["xclip", "-selection", "clipboard"], input=text.encode(), check=True)
-        else:
-            subprocess.run(["clip"], input=text.encode(), check=True)
-    except Exception as e:
-        print(f"Failed to copy to clipboard: {e}")
 
 def show_status_message(message, error=False):
     """ステータスラベルの更新"""
